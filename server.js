6a3db6f60eabf19da665b977d21241d6f619ae16
bronson-h <bdhousmans@gmail.com>
2023-10-25 18:25:18 -0700

    commit 2

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const express = require('express');
 const parser = require('body-parser');
 const app = express();
 const port = 3000;
+app.use(express.static('public_html'));
 app.use(parser.json());
 
 const db  = mongoose.connection;
@@ -15,8 +16,7 @@ const mongoDBURL = 'mongodb://127.0.0.1/ostaa';
 mongoose.connect(mongoDBURL, { useNewUrlParser: true });
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-var schema = new mongoose.Schema;
-var usernameSchema = new schema({
+var usernameSchema = new mongoose.Schema({
     username: String,
     password: String,
     listings: [],
@@ -24,7 +24,7 @@ var usernameSchema = new schema({
 });
 var userData = mongoose.model('username', usernameSchema);
 
-var itemSchema = new schema({
+var itemSchema = new mongoose.Schema({
     title: String,
     description: String,
     image: String,
@@ -34,13 +34,22 @@ var itemSchema = new schema({
 var itemData = mongoose.model('item', itemSchema);
 
 // gets users from database and returns them as a JSON array to client
-app.get('/get/users', (req,res) => {});
+app.get('/get/users', (req,res) => {
+    let users = userData.find({}).exec();
+    res.end(users.json());
+});
 
 // gets items from database and returns them as a JSON array to client
-app.get('/get/items/', (req,res) => {});
+app.get('/get/items/', (req,res) => {
+    let items = itemData.find({}).exec();
+    res.end(items.json());
+});
 
 // gets listings based on username from database and returns them as JSON array
-app.get('/get/listings/:username', (req,res) => {});
+app.get('/get/listings/:username', (req,res) => {
+    let userItems = itemData.find({}).exec(`${req.params.username}`);
+    res.end(userItems.json());
+});
 
 // gets purchases from database by certain username and returns JSON array
 app.get('/get/purchases/:username', (req,res) => {});
