8a93b070033cee38a7bf554217a4e9314a33d83c
Billy Dolny <wsdolny@arizona.edu>
2023-10-25 18:26:32 -0700

    Adding some get functions

--- server.js
+++ server.js
@@ -15,8 +15,7 @@ const mongoDBURL = 'mongodb://127.0.0.1/ostaa';
 mongoose.connect(mongoDBURL, { useNewUrlParser: true });
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-var schema = new mongoose.Schema;
-var usernameSchema = new schema({
+var usernameSchema = new mongoose.Schema({
     username: String,
     password: String,
     listings: [],
@@ -24,7 +23,7 @@ var usernameSchema = new schema({
 });
 var userData = mongoose.model('username', usernameSchema);
 
-var itemSchema = new schema({
+var itemSchema = new mongoose.Schema({
     title: String,
     description: String,
     image: String,
@@ -43,10 +42,52 @@ app.get('/get/items/', (req,res) => {});
 app.get('/get/listings/:username', (req,res) => {});
 
 // gets purchases from database by certain username and returns JSON array
-app.get('/get/purchases/:username', (req,res) => {});
+app.get('/get/purchases/:username', (req,res) => {
+    let name = req.params.username;
+    let query = userData.find({name})
+    query.then((documents) => {
+        user = documents[0];
+        console.log(user);
+        purchasesDataList = user.purchases;
+        purchaseList = [];
+        for (var i = 0; i < purchasesDataList; i++){
+            let item = purchasesDataList[i];
+            let jsonItem = {
+                title: item.title,
+                description: item.description,
+                image: item.image,
+                price: item.price,
+                status: item.status,
+            }
+            purchaseList.push(jsonItem);
+        }
+        res.end(purchaseList.json());
+    });
+});
 
 // returns JSON list of all usernames that contain keyword
-app.get('/search/users/:keyword', (req,res) => {});
+app.get('/search/users/:keyword', (req,res) => {
+    let word = req.params.keyword;
+    let query = itemData.find({word})
+    query.then((documents) => {
+        user = documents[0];
+        console.log(user);
+        listingDataList = user.listings;
+        listingList = [];
+        for (var i = 0; i < listingDataList; i++){
+            let item = listingDataList[i];
+            let jsonItem = {
+                title: item.title,
+                description: item.description,
+                image: item.image,
+                price: item.price,
+                status: item.status,
+            }
+            listingList.push(jsonItem);
+        }
+        res.end(listingList.json());
+    });
+});
 
 // returns JSON list of all items whose descriptions contain the keyword 
 app.get('/search/items/:keyword', (req,res) => {});
