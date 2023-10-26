9eb01706588f0b332a11c40b164c7d05c8e932b9
Billy Dolny <wsdolny@arizona.edu>
2023-10-25 17:34:55 -0700

    commit test/did the posts!

--- server.js
+++ server.js
@@ -5,8 +5,10 @@
 
 const mongoose = require('mongoose');
 const express = require('express');
+const parser = require('body-parser');
 const app = express();
 const port = 3000;
+app.use(parser.json());
 
 const db  = mongoose.connection;
 const mongoDBURL = 'mongodb://127.0.0.1/ostaa';
@@ -50,10 +52,46 @@ app.get('/search/users/:keyword', (req,res) => {});
 app.get('/search/items/:keyword', (req,res) => {});
 
 // adds a user to the database
-app.post('/add/user/', (req,res) => {});
+app.post('/add/user/', (req,res) => {
+
+    let name = req.body.username;
+    let pass = req.body.password;
+    let userData = new usernameSchema({username: name, password: pass});
+    userData.save()
+        .then(() => {
+            console.log(userData); // You can log the saved message here
+        })
+        .catch((error) => {
+            console.error("Error saving message:", error);
+            res.status(500).end("Error saving message.");
+        });
+});
 
 // adds item to database and adds item to username's list of items
-app.post('/add/item/:username', (req,res) => {});
+app.post('/add/item/:username', (req,res) => {
+    let pTitle = req.body.title;
+    let pDesc = req.body.description;
+    let pImg = req.body.image;
+    let pPrice = req.body.price;
+    let pStat = req.body.stat;
+    let pUser = req.body.username; 
+    let item = new itemData({title: pTitle, description: pDesc, image: pImg, price: pPrice, status: pStat});
+    item.save()
+        .then(() => {
+            console.log(item); // You can log the saved message here
+            let query = userData.find({pUser})
+            query.then((documents) => {
+                user = documents[0];
+                console.log(user);
+                let list = user.listings
+                list.push(item);
+            });
+        })
+        .catch((error) => {
+            console.error("Error saving message:", error);
+            res.status(500).end("Error saving message.");
+        });
+});
 
 app.listen(port, () => 
     console.log(`App listening at http://localhost:${port}`));
