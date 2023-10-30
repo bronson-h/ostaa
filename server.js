fca058a29998c6e64de010b0eff261ed31d623a8
bronson-h <bdhousmans@gmail.com>
2023-10-29 17:44:15 -0700

    updated port to be 80 instead of 3000

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const parser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = 80;
 app.use(express.static('public_html'));
 app.use(parser.json());
 
